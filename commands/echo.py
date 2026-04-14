bec459897739817bebef5d9ee5f55939a6e900cf
Taj Mace <taj.mac101@gmail.com>
2026-04-15 00:56:56 +1000

    FIX: fixed fstring parsing

--- commands/echo.py
+++ commands/echo.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# TOFIX: breaks when passed through quote marks inside the string:
+# eg. echo ""wow!"
+#           ^ this additional quote marks breaks the syntax
 def transpileEcho(groups: tuple[str, ...]) -> str:
-    return f"print({repr(groups[0])})"
+    return f'print(f"{groups[0]}")'
+
 
